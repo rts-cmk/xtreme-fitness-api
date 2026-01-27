e7e3c683e27f59d2c186514bd22c3d4bc517b308
carsten-l <clu@rts.dk>
2026-01-27 17:18:53 +0100

    add enpoints to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { Hono } from "hono";
-import { serveStatic } from '@hono/node-server/serve-static'
+import { serveStatic } from "@hono/node-server/serve-static";
 import assets from "./features/assets/api";
 import icons from "./features/icons/api";
 import benefits from "./features/benefits/api";
@@ -9,6 +9,8 @@ import services from "./features/services/api";
 import reviews from "./features/reviews/api";
 import memberships from "./features/memberships/api";
 import advantages from "./features/advantages/api";
+import messages from "./features/messages/api";
+import employees from "./features/employees/api";
 
 // error handling middleware
 import { errorHandler } from "./middleware/errorHandler";
@@ -18,9 +20,8 @@ app.use("*", cors());
 app.use("*", logger());
 app.use("*", errorHandler);
 
-
-app.use('/', serveStatic({ root: './public' }));
-app.use('/file-bucket/*', serveStatic({ root: './public' }));
+app.use("/", serveStatic({ root: "./public" }));
+app.use("/file-bucket/*", serveStatic({ root: "./public" }));
 
 app.route("/assets", assets);
 app.route("/benefits", benefits);
@@ -29,4 +30,7 @@ app.route("/icons", icons);
 app.route("/reviews", reviews);
 app.route("/memberships", memberships);
 app.route("/advantages", advantages);
+app.route("/messages", messages);
+app.route("/employees", employees);
+
 export default app;
