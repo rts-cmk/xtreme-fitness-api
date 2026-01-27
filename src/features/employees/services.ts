872ac421a11724e387a3dc7bcdf374595ececc1f
carsten-l <clu@rts.dk>
2026-01-27 17:17:32 +0100

    update employee db interaction

--- src/features/employees/services.ts
+++ src/features/employees/services.ts
@@ -2,13 +2,28 @@ import prisma from "../../../lib/prisma";
 import { NewEmployee } from "./validation";
 
 export async function createEmployee(data: NewEmployee) {
-    return prisma.employee.create({ data });
-  }
-  
-  export async function getAllEmployees() {
-  return prisma.employee.findMany();
-  }
+  return prisma.employee.create({
+    data: {
+      name: data.name,
+      area: data.area,
+      asset: {
+        connect: {
+          id: data.assetId,
+        },
+      },
+    },
+  });
+}
 
-  export async function deleteEmployee(id: number) {
-    return prisma.employee.delete({ where: { id } });
-  }
+export async function getAllEmployees() {
+  return prisma.employee.findMany({
+    include: {
+      asset: { omit: { id: true, createdAt: true } },
+    },
+    omit: { assetId: true },
+  });
+}
+
+export async function deleteEmployee(id: number) {
+  return prisma.employee.delete({ where: { id } });
+}
