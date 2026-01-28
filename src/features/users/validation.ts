1a9dcb20cb399b812b049c566c0a558db07efbca
carsten-l <clu@rts.dk>
2026-01-28 16:31:52 +0100

    include membershipid and role in schema and type

--- src/features/users/validation.ts
+++ src/features/users/validation.ts
@@ -4,7 +4,10 @@ export const userSchema = z.object({
     id: z.number().optional(),
     name: z.string().min(2),
     email: z.string().email(),
+    role: z.enum(['member', 'admin']).optional(),
+    membershipId: z.number().optional(),
     password: z.string().min(6),
+    newPassword: z.string().min(6).optional(),
 });
 
 export type User = z.infer<typeof userSchema>;
