f0bcf3dfb7f970fe1e02489309cb415c32eb733e
carsten-l <clu@rts.dk>
2026-01-28 16:32:49 +0100

    update included data on /me

--- src/features/users/services.ts
+++ src/features/users/services.ts
@@ -1,22 +1,32 @@
 import prisma from "../../../lib/prisma"; 
 import { hashSync } from "bcryptjs";
+import type { NewUser } from "./validation";
 
 export async function getUserById(id: number) {
   const user = await prisma.user.findUnique({ 
     where: { id }, 
-    include: { bookings: true, comments: true }
+    include: { 
+      enrolledWorkouts: true, 
+      membership: {
+        omit: { id: true, assetId: true, createdAt: true },
+        include: { asset: {omit: {id: true, createdAt: true} } }
+      } 
+  },
+    omit: { membershipId : true }
   });
   if (!user) return null;
   const { password, ...userWithoutPassword } = user;
   return userWithoutPassword;
 }
 
-export async function updateUser(id: number, data: { name?: string; email?: string; newPassword?: string }) {
+export async function updateUser(id: number, data: NewUser ) {
   const user = await prisma.user.update({
     where: { id },
     data: {
       name: data.name,
       email: data.email,
+      membershipId: data.membershipId,
+      role: data.role,
       password: data.newPassword ? hashSync(data.newPassword, 15) : undefined,
     },
   });
